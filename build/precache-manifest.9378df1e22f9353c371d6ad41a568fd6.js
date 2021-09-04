1e988d9bee044f310b87ccc8c7a2aedc1ad36975
17021072 <daohuyentrang091999@gmail.com>
2021-09-04 22:14:07 +0700

    update

---
+++ build/precache-manifest.9378df1e22f9353c371d6ad41a568fd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d27a43990c52c367d2af63d5ae45d73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c10470f9eb79fca9fb3",
+    "url": "/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "a1a1349832653a05357e",
+    "url": "/static/css/main.62626c26.chunk.css"
+  },
+  {
+    "revision": "1c10470f9eb79fca9fb3",
+    "url": "/static/js/2.2c19de11.chunk.js"
+  },
+  {
+    "revision": "a3ca4fad6c20f8df27c31f653f9e89e9",
+    "url": "/static/js/2.2c19de11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a1349832653a05357e",
+    "url": "/static/js/main.29bd4d3c.chunk.js"
+  },
+  {
+    "revision": "fe43d60bbb01d32c51d1",
+    "url": "/static/js/runtime-main.a3a8a1a1.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,118 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Router, Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Login from "./components/login";
+import Register from "./components/register";
+import Home from "./components/home";
+import Profile from "./components/profile";
+import { logout } from "./actions/auth";
+import { clearMessage } from "./actions/message";
+import { history } from './helpers/history';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+
+    this.state = {
+      currentUser: undefined,
+    };
+
+    history.listen((location) => {
+      props.dispatch(clearMessage()); // clear message when changing location
+    });
+  }
+
+  componentDidMount() {
+    const user = this.props.user;
+
+    if (user) {
+      this.setState({
+        currentUser: user,
+      });
+    }
+  }
+
+  logOut() {
+    this.props.dispatch(logout());
+  }
+
+  render() {
+    const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
+
+    return (
+      <Router history={history}>
+        <div>
+          <nav className="navbar navbar-expand navbar-dark bg-dark">
+            
+            <div className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to={"/home"} className="nav-link">
+                  Home
+                </Link>
+              </li>
+
+             {currentUser && (
+                <li className="nav-item">
+                  <Link to={"/user"} className="nav-link">
+                    User
+                  </Link>
+                </li>
+              )}
+            </div>
+
+            {currentUser ? (
+              <div className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link to={"/profile"} className="nav-link">
+                    {currentUser.username}
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <a href="/login" className="nav-link" onClick={this.logOut}>
+                    LogOut
+                  </a>
+                </li>
+              </div>
+            ) : (
+              <div className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link to={"/login"} className="nav-link">
+                    Login
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link to={"/register"} className="nav-link">
+                    Sign Up
+                  </Link>
+                </li>
+              </div>
+            )}
+          </nav>
+
+          <div className="container mt-3">
+            <Switch>
+              <Route exact path={["/", "/home"]} component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/profile" component={Profile} />
+              
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const { user } = state.auth;
+  return {
+    user,
+  };
+}
+
+export default connect(mapStateToProps)(App);
